141d8d20014bc30a83273bbaf77bd09b0299274b
yujeonglee623 <yujeonglee623@gmail.com>
2025-12-02 14:33:23 +0900

    파일 수정

--- ml_deploy/app_st.py
+++ ml_deploy/app_st.py
@@ -42,7 +42,7 @@ st.title("Iris 품종 예측")
 st.write("꽃받침 길이, 너비, 꽃잎 길이, 너비를 입력하여 품종을 예측해보세요.")
 
 # 이미지 로딩
-img_path = "static/flower1.jpg"
+img_path = "ml_deploy/static/flower1.jpg"
 st.image(img_path, caption="꽃")
 
 # 사용자 입력 받기
